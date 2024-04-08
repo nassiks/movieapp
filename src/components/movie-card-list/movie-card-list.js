08d491953057a4cfd5229370fae63251f882301c
Anastasia <ana.gordeewa@bk.ru>
2024-04-08 19:40:50 +0300

    Второе задание сделано. Добавлено: обработка ошибок, индикатор загрузки, ошибка при отсутсвии сети

--- src/components/movie-card-list/movie-card-list.js
+++ src/components/movie-card-list/movie-card-list.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-import { List } from 'antd'
+import { List, Spin, Alert } from 'antd'
 
 import MovieCard from '../movie-card'
 import MovieServices from '../../services/movie-services'
+
 import './movie-card-list.css'
 
 export default class MovieCardList extends Component {
   state = {
     movies: [],
+    loading: true,
+    error: null,
   }
 
   movieService = new MovieServices()
@@ -17,15 +20,41 @@ export default class MovieCardList extends Component {
   }
 
   getMovies() {
-    this.movieService.getAllMovie().then((movies) => {
-      this.setState({
-        movies,
+    this.movieService
+      .getAllMovie()
+      .then((movies) => {
+        this.setState({
+          movies,
+          loading: false,
+        })
       })
-    })
+      .catch(() => {
+        this.setState({
+          error: 'Не удается загрузить данные. Пожалуйста, проверьте соединение с интернетом.',
+          loading: false,
+        })
+      })
   }
 
   render() {
-    const { movies } = this.state
+    const { movies, loading, error } = this.state
+
+    if (error) {
+      return (
+        <div className="movie-list-container">
+          <Alert message="Ошибка!" description={error} type="error" showIcon />
+        </div>
+      )
+    }
+
+    if (loading) {
+      return (
+        <div className="movie-list-container">
+          <Spin size="large" />
+        </div>
+      )
+    }
+
     return (
       <div className="movie-list-container">
         <List
